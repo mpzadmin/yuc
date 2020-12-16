8a9495cfc45bcabc7fd2baf6a8d33a3880df23b3
Aliasghar Afzalabadi <aliasgharafzalabadi@gmail.com>
2020-12-16 13:27:03 +0330

    afzalabadi

--- Afzalabadi/base.cpp
+++ Afzalabadi/base.cpp
@@ -1,8 +1,23 @@
 #include <iostream>
+#include<string>
 
 using namespace std;
+void delay();
+
 
 int main()
 {
-    
+    string charector;
+    cout << "Enter your charextor:";
+    getline(cin, charector);
+    for(int i=0; i<charector.length(); i++)
+    {
+        void delay();
+        cout <<  charector[i] ;
+
+    }   
+void delay(int delay);
+{
+    for(int index =1; index<=100000; index++);
 }
+

5b366cb4743b52b1eec31d160bbb1cec27cfd436
Ali Khaleqi Yekta <Me@Yekta.Dev>
2020-12-16 10:56:47 +0330

    Update KhaleqiYekta/delay.cpp

--- KhaleqiYekta/delay.cpp
+++ KhaleqiYekta/delay.cpp
@@ -1,15 +1,32 @@
-#include <iostream>
-#include <string.h>
+/*
+ *  Print Animator Program
+ * 
+ *  By: YektaDev [Ali Khaleqi Yekta]
+ *  Website: Yekta.Dev
+ *  Email: Me@Yekta.Dev
+ */
 
+
+// External Libraries
+#include <iostream>
+#include <string>
+
+// Namespaces
 using namespace std;
 
+// Constants
 const int delayValue = 50;
 const string textToPrint = "Hello C++ Class!";
 
-void delay(int);
+// Function Declrations
+void clearConsole();
+void delay(int delay = 10);
 
+// Program's Entry Point
 int main()
 {
+    clearConsole();
+
     for (int i = 0; i < textToPrint.length(); i++)
     {
         delay(delayValue);
@@ -18,10 +35,22 @@ int main()
     }
 }
 
+// Block the thread for some time
 void delay(int delay)
 { 
     for (int i = 0; i < 1000000; i++)
     {
         for (int j = 0; j < delay; j++);
     }
+}
+
+
+// Clear the console (on any OS)
+void clearConsole()
+{ 
+    #ifdef _WIN32
+    system("cls");
+    #else
+    system("clear");
+    #endif
 }

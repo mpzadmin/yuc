4b2dcb54d31d739b17045b25bbeb39df4bdc8c91
Ahad RahimiPour <ahadrahimipour@gmail.com>
2020-12-16 09:04:35 +0330

    Add delay

---
+++ Rahimipour/test57.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+void display(string str);
+
+int main()
+{
+    display("Hello");
+    display("Welcome");
+}
+
+void display(string str)
+{
+    cout << str;
+}

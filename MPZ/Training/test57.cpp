abcbf05887f3cc7a26cebc89e36bdab614c586b0
Masoud PourZahmatkesh <mpzadmin@gmail.com>
2020-12-16 09:11:57 +0330

    Update test57.cpp and fix an issue related to function declaration

--- MPZ/Training/test57.cpp
+++ MPZ/Training/test57.cpp
@@ -3,18 +3,20 @@
 
 using namespace std;
 
-void display(string str = "Hello World!", bool newLine = false)
-{
-    cout << str;
-    if (newLine)
-        cout << endl; 
-}
+void display(string str = "Hello World!", bool newLine = false);
 
 int main()
 {
     display("Hello", true);
     display("Welcome", true);
     display("Bye");       
+}
+
+void display(string str, bool newLine)
+{
+    cout << str;
+    if (newLine)
+        cout << endl; 
 }
 
 

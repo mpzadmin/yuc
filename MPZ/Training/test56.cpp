7ed3e8af7360b70a564943afd97f800f8b5ec648
Masoud PourZahmatkesh <mpzadmin@gmail.com>
2020-12-16 08:36:41 +0330

    Add test56.cpp

---
+++ MPZ/Training/test56.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+using namespace std;
+
+// Function declaration
+void helloWorld();
+
+int main()
+{
+    // Function call
+    helloWorld();
+    helloWorld();
+    helloWorld();
+    helloWorld();    
+}
+
+// Function definition
+void helloWorld()
+{
+    cout << "Hello world from C++";
+}

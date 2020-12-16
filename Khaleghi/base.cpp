c76e2c6546b0e427de7fd6b5a487db832436d33b
Ali Khaleqi Yekta <Me@Yekta.Dev>
2020-12-16 10:28:52 +0330

    Update Ali Khaleqi Yekta's base.cpp

--- Khaleghi/base.cpp
+++ Khaleghi/base.cpp
@@ -1,17 +1,38 @@
-// By: Ali Khaleqi Yekta
+/*
+ *  [PROGRAM NAME]
+ * 
+ *  By: YektaDev [Ali Khaleqi Yekta]
+ *  Website: Yekta.Dev
+ *  Email: Me@Yekta.Dev
+ */
 
+// External Libraries
 #include <iostream>
-#include <string.h>
+#include <string>
 
+// Namespaces
 using namespace std;
 
+// Constants
+const string defaultStringRequestTitle = "Please enter a text: ";
+const string defaultIntRequestTitle = "Please enter a number: ";
+
+// Function Declrations
+void clearConsole();
+string askForString(string titleToPrint = defaultStringRequestTitle);
+int askForInt(string titleToPrint = defaultIntRequestTitle);
+
+// Program's Entry Point
 int main()
 {
     clearConsole();
 
-
+    /* CODE GOES HERE */
 }
 
+/* PROGRAM FUNCTIONS GO HERE */
+
+// Clear the console (on any OS)
 void clearConsole()
 { 
     #ifdef _WIN32
@@ -19,4 +40,28 @@ void clearConsole()
     #else
     system("clear");
     #endif
+}
+
+// Return a string which is requested from user, then goes to a new line after the procces is done.
+string askForString(string titleToPrint = defaultStringRequestTitle)
+{
+    string data;
+
+    cout << titleToPrint;
+    getline(cin, data);
+    cout << endl;
+
+    return data;
+}
+
+// Return an integer which is requested from user, then goes to a new line after the procces is done.
+int askForInt(string titleToPrint = defaultIntRequestTitle)
+{
+    int data;
+
+    cout << titleToPrint;
+    cin >> data;
+    cout << endl;
+
+    return data;
 }

4b3948b3c5cb5021176ffa499ef16ec37225095d
ALIREZA EFTEKHARI <alireza1380.eftekhari1380@gmail.com>
2020-12-17 14:59:50 +0330

    Add delay

--- Eftekhari/test1.cpp
+++ Eftekhari/test1.cpp
@@ -5,21 +5,21 @@ using namespace std ;
 void delay(int delayvalue = 30);
 int main(int argc, char const *argv[])
 {
-    string name = "ALIREZA EFTEKHARI" ;
+    string name = "ALIREZA EFTEKHARI!" ;
     for (int index = 0; index < name.length(); index++)
     {
         cout << name[index] ;
         delay( ) ;
     }
     
-    return 0;
+    return 0 ;
 }
 
 void delay(int delayvalue)
 {
     for (int index = 0; index < delayvalue; index++)
     {
-        for(int counter = 0; counter < 2000000; counter++);
+        for(int counter = 0; counter < 1500000; counter++);
     }
     
 }

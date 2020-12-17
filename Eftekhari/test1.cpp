4ad2e9df9e8e8246559bc4495611f926b0080771
ALIREZA EFTEKHARI <alireza1380.eftekhari1380@gmail.com>
2020-12-17 14:29:24 +0330

    Add test1.cpp

---
+++ Eftekhari/test1.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+using namespace std ;
+
+void delay(int delayvalue = 30);
+int main(int argc, char const *argv[])
+{
+    string name = "ALIREZA EFTEKHARI" ;
+    for (int index = 0; index < name.length(); index++)
+    {
+        cout << name[index] ;
+        delay( ) ;
+    }
+    
+    return 0;
+}
+
+void delay(int delayvalue)
+{
+    for (int index = 0; index < delayvalue; index++)
+    {
+        for(int counter = 0; counter < 2000000; counter++);
+    }
+    
+}
+

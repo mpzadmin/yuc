655b9b29cce29907b202591b446ab4c02ad86819
Hossein Pakparvar <hp.pakparvar@gmail.com>
2020-12-19 09:17:28 +0330

    test2.cpp

---
+++ Pakparvar/test2.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+void display(string str);
+void display(string str, bool newLine);
+ 
+int main()
+{
+    display("Hello", true);
+    display("Welcome");
+    display("Bye", true);
+}
+
+void display(string str)
+{
+    cout << str;
+}
+
+void display(string str, bool newLine)
+{
+    display(str);
+    if(newLine)
+    {
+        cout << endl;
+    }
+}

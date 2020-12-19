e7fabde4bc26f23e2ab5e01ad912700cfefd6259
Masoud PourZahmatkesh <mpzadmin@gmail.com>
2020-12-19 08:57:42 +0330

    Add test60.cpp and test61.cpp

---
+++ MPZ/Training/test62.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+void display(string str);
+void display(string str, bool newLine);
+
+int main()
+{
+    display("Hello", true);
+    display("Welcome");
+    display("Bye",true);    
+}
+
+void display(string str)
+{
+    cout << str;
+}
+
+void display(string str, bool newLine)
+{
+    cout << str;
+    if (newLine)
+    {
+        cout << endl;
+    }
+}

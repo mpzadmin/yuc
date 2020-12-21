52403dc67a83de2b4ab9b4d0730d4176cb67651f
hossein nikbakht <hossein.nikbakht092100@gmail.com>
2020-12-21 05:40:42 +0330

    delay function

---
+++ Nikbakht/test1.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+#include<string> 
+#include<iomanip>
+#include<conio.h>
+using namespace std;
+
+void delay(int index);
+int main()
+{
+    string str;
+    cout << "enter your name:";
+    getline(cin , str);
+    for( int index3 = 0 ; index3 < str.length() ; index3++)
+    {
+        cout << str[index3];
+        delay(20000000);
+    }
+
+}
+void delay(int index)
+{
+    
+    for(int index2 =0 ; index2 < index ; index2++)
+    {
+
+    }
+    
+}

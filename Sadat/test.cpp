4924ae262f580ac10c2a1c1ff28f41467a3001b8
Pouya Sadat alhoseini <pouya.s2781@gmail.com>
2020-12-21 23:32:27 -0800

    Adding test

--- Sadat/test.cpp
+++ Sadat/test.cpp
@@ -2,7 +2,12 @@
 
 using namespace std;
 
-int main()
+int main(int argc, const char** argv) 
 {
-    
+    if (argc <= 1)
+    {
+        cout << "You have to enter a name while calling the programm!" << endl;
+        return 0;
+    }
+    cout << "Hello " << argv[1] << endl;
 }

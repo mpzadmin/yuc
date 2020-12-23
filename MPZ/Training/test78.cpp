3aaf4f5afe5d47abe2454870f52fb1fd7d9fd70c
Masoud PourZahmatkesh <mpzadmin@gmail.com>
2020-12-23 08:55:52 +0330

    Add test78.cpp, dynamic 2d array using pointers

--- MPZ/Training/test78.cpp
+++ MPZ/Training/test78.cpp
@@ -2,6 +2,8 @@
 
 using namespace std;
 
+void printNumbers(int** numbers, size_t rowCount, size_t colCount);
+
 int main()
 {
     int** numbers;
@@ -14,7 +16,34 @@ int main()
     numbers = new int* [rowCount];
     for (int row=0; row<rowCount; row++)
     {
-        cout << "";
+        cout << "Enter count of columns of row (" << row+1 << "): ";
+        cin >> colCount;
+        numbers[row] = new int [colCount];
+        for (int col=0; col<colCount; col++)
+        {
+            cout << "Enter column (" << col+1 << ") of row (" << row+1 << "): ";
+            cin >> numbers[row][col];
+        }
     }
 
+    printNumbers(numbers, rowCount, colCount);
+
+    for (int row=0; row<rowCount; row++)
+    {
+        delete[] numbers[row];
+    }
+    
+    delete[] numbers;
+}
+
+void printNumbers(int** numbers, size_t rowCount, size_t colCount)
+{
+    for (int row=0; row<rowCount; row++)
+    {        
+        for (int col=0; col<colCount; col++)
+        {
+            cout << numbers[row][col] << "\t";
+        }
+        cout << endl;
+    }
 }

148b3d6c27c37e8b5857583f6069039873168278
Omid paktinat <omidpak2@gmail.com>
2020-12-24 18:31:45 +0330

    positive number

--- Paktinat/test2.cpp
+++ Paktinat/test2.cpp
@@ -4,10 +4,11 @@ using namespace std;
 
 int main ()
 {
-    int number, factorial=1;
-    cout << "enter a number : ";
+    int unsigned number; 
+    int factorial=1;
+    cout << "enter a positive number : ";
     cin >> number;
-     if ( number == 0 )
+     if ((number == 0) || (number<0))
         {
             cout << "error" << endl;
             return 0;
@@ -15,8 +16,7 @@ int main ()
     for (int i=1; i<=number; i++)
     {
         factorial *= i;
-       
     }
-
     cout << factorial;
+    return 0;
 }

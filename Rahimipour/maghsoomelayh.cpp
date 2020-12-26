5dd357cfa233a06c82f62743176a83ca9b482d62
Ahad RahimiPour <ahadrahimipour@gmail.com>
2020-12-26 07:11:53 +0330

    Add maghsoomelayh.cpp for showing all divisor

---
+++ Rahimipour/maghsoomelayh.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+int main()
+{
+    system("cls");
+
+    HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+    int number;
+    SetConsoleTextAttribute(consoleHandle, 1);
+    cout << "Enter a number: ";
+    cin >> number;
+    int list[number];
+    
+    for (int index = 1; index <= number; index++)
+    {
+        if (number % index == 0)
+        {
+            list[index] = (number / index);
+        }
+    }
+
+    for (int index = number; index >= 1; index--)
+    {
+        if (list[index] * index == number)
+        {
+            SetConsoleTextAttribute(consoleHandle, 2);
+            cout << list[index] << ", ";
+        }
+    }
+}

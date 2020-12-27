17110880416449a36a0b22134210ce16c2ee814c
Omid paktinat <omidpak2@gmail.com>
2020-12-28 01:08:13 +0330

    add color

---
+++ Paktinat/test7.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  int a=500000;
+  string name;
+  cout << "enter a name : ";
+  getline(cin, name);
+  system("cls");
+  for ( int i=0; i<=name.length(); i++ )
+  {
+    cout << name[i];
+     for ( int i = 1; i <= a*a; i++ )
+      {
+          
+      }
+  }
+  return 0;
+ 
+
+
+
+}

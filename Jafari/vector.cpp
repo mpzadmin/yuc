5ef0d4d59dba3d5f4adcb7b049d07862bd66c26a
Arman Jafari <armanjafari616@outlook.com>
2020-12-29 22:44:54 +0330

    add vector practice

---
+++ Jafari/vector.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, const char** argv) 
+{
+    int count, number, average;
+    int sum = 0;
+
+    vector<int> numbers;
+
+    system("cls");
+    cout << "Enter count of numbers: ";
+    cin >> count;
+
+    // get numbers
+    for (int index = 0; index < count; index++)
+    {
+        cout << "Enter number " << (index + 1) << " : ";
+        cin >> number;
+        numbers.push_back(number);
+    }
+
+    // printing numbers
+    cout << endl << "Your numbers are: ";
+    for (int index = 0; index < count; index++)
+    {
+        cout << numbers.at(index) << "\t";
+    }
+    cout << endl << endl;
+
+    // sum numbers
+    for (int index = 0; index < count; index++)
+    {
+        sum += numbers.at(index);
+    }
+
+    // calculate average
+    average = sum / count;
+    cout << "Average = " << average << endl;
+
+    int max = numbers.front();
+    int min = numbers.front();
+
+    // calculatimg Maximum and Minimum
+    for (int index = 0; index < count; index++)
+    {
+        if (numbers.at(index) > max)
+            max = numbers.at(index);
+
+        if (numbers.at(index) < min)
+            min = numbers.at(index);
+    }
+
+    cout << "Maximum = " << max << "\nMinimum = " << min;
+
+    return 0;
+}

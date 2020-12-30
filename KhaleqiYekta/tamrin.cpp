d2ae81955bd4d8e8b6aa3b603bdc1482e8735705
Ali Khaleqi Yekta <Me@Yekta.Dev>
2020-12-30 09:41:41 +0330

    Create tamrin.cpp

---
+++ KhaleqiYekta/tamrin.cpp
@@ -0,0 +1,68 @@
+/*
+ * Copyright © 2020 Ali Khaleqi Yekta, All Rights Reserved.
+ *
+ * Author: Ali Khaleqi Yekta [YektaDev]
+ * Website: https://Yekta.Dev
+ * Email: Me@Yekta.Dev
+ * Creation Date: 2020-12-30
+ */
+
+/** External Libraries **/
+#include <iostream>
+#include <string>
+#include <list>
+#include <algorithm>
+#include "console.hpp"
+
+/** Namespaces **/
+using namespace std;
+
+/** Function Declarations **/
+bool search(const list<string> names, const string name);
+
+// Program's Entry Point
+int main()
+{
+    /** Initialization Of Local Variables **/
+    list<string> names;
+    string name;
+    size_t namesCount;
+
+    clearConsole();
+
+    namesCount = askForInt("Enter the count of names: ");
+
+    for (size_t i = 0; i < namesCount; ++i)
+    {
+        name = askForString("Enter name " + to_string(i + 1) + ": ");
+        names.push_back(name);
+    }
+
+    name = askForString("Enter a name to search in names: ");
+
+    if (search(names, name))
+    {
+        printLine(name + " was found!");
+    }
+    else
+    {
+        printLine(name + " was not found!");
+    }
+
+    /** End of the program **/
+    pauseConsole();
+    return 0;
+}
+
+bool search(const list<string> names, const string name)
+{
+    for (string n : names)
+    {
+        if (n == name)
+        {
+            return true;
+        }
+    }
+
+    return false;
+}

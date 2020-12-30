7937ed9683dc79a0064b5405e7ea2acae5fd0171
Ahad RahimiPour <ahadrahimipour@gmail.com>
2020-12-30 07:59:59 +0330

    Add averagemax.cpp, for computing average and max and min, by another way.

---
+++ Rahimipour/test73.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <math.h>
+
+using namespace std;
+
+int main()
+{
+    cout << sqrt(64) << endl;
+    cout << round(134.43) << endl;
+    cout << round(4.6) << endl;
+    cout << abs(-313) << endl;
+    cout << log(2) << endl;
+}

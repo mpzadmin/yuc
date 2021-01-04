d64c2a1c2141822cad470335ada6b50419793fed
Masoud PourZahmatkesh <mpzadmin@gmail.com>
2021-01-04 08:32:41 +0330

    Add a few samples about variables and files, test106.cpp, test108.cpp, test108.hpp

--- MPZ/Training/test106.cpp
+++ MPZ/Training/test106.cpp
@@ -1,8 +1,27 @@
 #include <iostream>
+#include <fstream>
 
 using namespace std;
 
 int main()
 {
     
+    int number = 0;
+    int* ptr; 
+
+    for (;;)
+    {
+        number++;
+    }
+
+    //
+    //
+    ptr = new int [1];
+    //
+    //
+
+    delete[] ptr;
+    //
+    //
+
 }

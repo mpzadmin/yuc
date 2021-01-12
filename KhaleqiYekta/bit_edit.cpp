ff22b071df18cfe788f113cbe14c1c26b1fef95a
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-01-12 11:04:37 +0330

    Add bit_edit.cpp

---
+++ KhaleqiYekta/bit_edit.cpp
@@ -0,0 +1,29 @@
+/*
+ * Copyright © 2021 Ali Khaleqi Yekta, All Rights Reserved.
+ *
+ * Author: Ali Khaleqi Yekta [YektaDev]
+ * Website: https://Yekta.Dev
+ * Email: Me@Yekta.Dev
+ */
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    int num;
+
+    system("cls");
+
+    cout << "Enter a number: ";
+    cin >> num;
+
+    num |= 0100001011000;
+    num &= 00111111011;
+
+    cout << endl << "Result: " << num << endl;
+
+    return 0;
+}

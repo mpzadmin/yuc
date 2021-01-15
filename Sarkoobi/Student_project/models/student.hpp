cf6f38dbfa60185cdbe098e31290395c91404d30
mojtaba sarkoobi <moj.sarkoobi@gmail.com>
2021-01-16 02:28:12 +0330

    add students constructor

--- Sarkoobi/Student_project/models/student.hpp
+++ Sarkoobi/Student_project/models/student.hpp
@@ -4,10 +4,35 @@
 
 using namespace std;
 
-class StudentModel1
+class StudentModel
 {
     public:
         int code;
         string name;
         float average;
+};
+
+class Student
+{
+    private:
+        StudentModel studentmodel;
+        list<StudentModel> students;
+
+        bool error;
+    protected:
+
+    public:
+        Student();
+        ~Student();
+};
+
+Student::Student()
+{
+    this->students.clear();
+    this->error = false;
+}
+
+Student::~Student()
+{
+    
 }

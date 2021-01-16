bc9db4bdbc3bb894857bea5e64002fab7ff329af
pouya sadat <pouya.s2781@gmail.com>
2021-01-16 08:47:06 +0330

    An Update on student project

--- Sadat/student/models/student.hpp
+++ Sadat/student/models/student.hpp
@@ -6,8 +6,9 @@ using namespace std;
 class StudentModel
 {
     public:
-        string name;
+        int id;
         int code;
+        string name;
         float average;
 };
 
@@ -22,6 +23,14 @@ class Student
     public:
         Student();
         ~Student();
+
+        Student* setCode(int code);
+        Student* setName(string name);
+        Student* setAverage(float average);
+        int getCode();
+        string getName();
+        float getAverage();
+
         bool fail();
         string getError();
 };
@@ -45,4 +54,37 @@ bool Student::fail()
 string Student::getError()
 {
     return (this->error) ? this->errorMessage : "";
+}
+
+Student* Student::setCode(int code)
+{
+    this->studentModel.code = code;
+    return this;
+}
+
+Student* Student::setName(string name)
+{
+    this->studentModel.name = name;
+    return this;
+}
+
+Student* Student::setAverage(float average)
+{
+    this->studentModel.average = average;
+    return this;
+}
+
+int Student::getCode()
+{
+    return this->studentModel.code;
+}
+
+string Student::getName()
+{
+    return this->studentModel.name;
+}
+
+float Student::getAverage()
+{
+    return this->studentModel.average;
 }

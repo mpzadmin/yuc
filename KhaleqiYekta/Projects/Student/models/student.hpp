3572f2515a4f2f06af5a66ffb24b5fb5151e9a9d
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-01-16 08:51:48 +0330

    Update student project

--- KhaleqiYekta/Projects/Student/models/student.hpp
+++ KhaleqiYekta/Projects/Student/models/student.hpp
@@ -16,23 +16,32 @@ using namespace std;
 class StudentModel
 {
     public:
-    int code;
-    string name;
-    float average;
+        int code;
+        string name;
+        float average;
 };
 
 class Student
 {
     private:
-    StudentModel studentModel;
-    list<StudentModel> students;
-
-    bool error;
+        StudentModel studentModel;
+        list<StudentModel> students;
+        bool error;
 
     protected:
     public:
-    Student();
-    ~Student();
+        Student();
+        ~Student();
+
+        Student* setCode(int code);
+        int getCode();
+        Student* setName(string name);
+        string getName();
+        Student* setAverage(float avarage);
+        float getAverage();
+
+        bool fail();
+        string getError();
 };
 
 Student::Student()
@@ -44,4 +53,36 @@ Student::Student()
 Student::~Student()
 {
 
+}
+
+Student* Student::setCode(int code)
+{
+    this->studentModel.code = code;
+    return this;
+}
+
+int Student::getCode()
+{
+    return studentModel.code;
+}
+
+Student* Student::setName(string name)
+{
+    this->studentModel.name =name;
+    return this;
+}
+
+string Student::getName(int code)
+{
+    return studentModel.name;
+}
+
+bool Student::fail()
+{
+    return this->error;
+}
+
+bool Student::getError()
+{
+    return this->error ? this->errorMessage : "";
 }

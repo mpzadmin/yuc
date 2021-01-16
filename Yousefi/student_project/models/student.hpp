8c14bbc800510e6b8f2c5065eda3f3df75d2f7d9
habib allah yousofi <yousofihabib61@gmail.com>
2021-01-16 10:42:17 +0330

    Initial commit

---
+++ Yousefi/student_project/models/student.hpp
@@ -0,0 +1,126 @@
+#include <iostream>
+#include <list>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+
+class StudentModel
+{
+
+    public:
+        int code;
+        string name;
+        float average;
+
+};
+
+class Student
+{
+    private:
+        StudentModel studentmoel;
+        list<StudentModel> students;
+
+        bool error;
+
+        string errorMessage;
+    public:
+        Student();
+       ~Student(); 
+       void debug();
+
+       Student* setcode(int code);
+       int getcode();
+
+       Student* setname(string name);
+       string getname();
+
+       Student* setAverage(float average);
+       float getAverage(); 
+
+
+       Student* list();
+
+       bool fail();
+       string getError();  
+};  
+
+Student :: Student()
+{
+    this->students.clear();
+    this->error = false;
+}
+Student :: ~Student()
+{
+
+} 
+
+void Student::debug()
+{
+    cout << endl;
+    cout << "****************" <<endl;
+    cout << "code: "<<this->getcode()<<endl;
+    cout << "name: "<<this->getname() <<endl;
+    cout << "average: "<<this->getAverage << endl;
+    cout << "************" <<endl;
+}
+
+bool Student::fail()
+{
+    return this->error;
+}
+
+string Student::getError()
+{
+    return this->error ? this->errorMessage : "";
+}
+
+Student* Student:: setcode(int code)
+{
+    this->studentModel.code=code;
+    return this
+}
+
+int Student:: getcode()
+{
+    return this->studentModel.code;
+}
+
+Student* Student::setname(string name)
+{
+    this->studentModel.name=name;
+    return this;
+}
+string Student::getname()
+{
+    return this->studentModel.name;
+}
+
+Student* Student::getaverage(float average)
+{
+    this->studentModel.average=average;
+    return this;
+}
+float Student::getaverage()
+{
+    return this->studentModels.average;
+}
+
+Student* Student :: list();
+{
+    if (this->student.size() <= 0)
+    {
+        return this;
+    }
+    for (StudentModel stu : this -> students)
+    {
+        cout << "code: " << stu.code << endl;
+        cout << "name: " << stu.name << endl;
+        cout << "average: " << stu.average << endl;
+        cout << endl;
+    }
+    
+    return this;
+}
+ 

dbf350ba1dde4ed5f7c027876d79be2514ab37fa
Ahad RahimiPour <ahadrahimipour@gmail.com>
2021-01-18 07:36:07 +0330

    Change some method in main.cpp

--- Rahimipour/Student_project/main.cpp
+++ Rahimipour/Student_project/main.cpp
@@ -16,7 +16,7 @@ int main()
     student->setCode(1)->setName("Ahad")->setAverage(19.5)->debug();
     student->setCode(2)->setName("Ali")->setAverage(16)->debug();
     student->setCode(3)->setName("Taha")->setAverage(19.75)->add();
-    student->list();
+    //student->list();
 
     delete student;
 }

5d7d9f4c2fca0d3085254fe0f42f8034417605ab
Hossein Pakparvar <hp.pakparvar@gmail.com>
2021-01-18 10:50:43 +0330

    Update files

--- Pakparvar/Student/main.cpp
+++ Pakparvar/Student/main.cpp
@@ -13,6 +13,23 @@ int main()
     student->setCode(1)->setName("Hossein")->setAverage(19)->add();
     student->setCode(2)->setName("Ali")->setAverage(20)->add();
     student->list();
+    student->setCode(2)->remove()->list();
+
+    if (student->setCode(2)->remove()->fail())
+    {
+        cout << student->getError() << endl;
+    }
+
+    if(student->setCode(10)->setName("ali")->setAverage(20)->add()->fail())
+    {
+        cout << "Success" << endl;
+    }
+    else
+    {
+        cout << student->getError() << endl;
+    }
+    
+
     if(student->setName("Omid")->find(Field::Name))
     {
         student->debug();
@@ -21,8 +38,15 @@ int main()
     {
         cout << "NOT FOUND";
     }
+
+    if (student->setAverage(16)->find(Field::Average))
+    {
+        student->debug();
+    }
+    else
+    {
+        cout << "NOT FOUND!";
+    }
     
-
-
     delete student;
 }

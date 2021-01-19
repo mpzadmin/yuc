36a8812ca20052faa3511a98e886b3cfdf7415d6
Ali Khaleqi Yekta <Me@Yekta.Dev>
2021-01-19 11:18:54 +0330

    Add a 'filter' test in main.cpp

--- KhaleqiYekta/Projects/Student/main.cpp
+++ KhaleqiYekta/Projects/Student/main.cpp
@@ -50,6 +50,10 @@ int main()
         cout << student->getError() << endl;
     }
 
+    c::printlnSeparator('*',40);
+    student->setAverage(20)->filter(Field::Average)->list(true);
+    c::printlnSeparator('*',40);
+
 
     delete student;
 

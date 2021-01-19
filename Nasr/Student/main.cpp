fc7ea7868221447dd351ad01c8f57dae7a3e57af
Mohammad mahdi nasr <nasrmoammad4804.com>
2021-01-20 01:05:11 +0330

    add a filter and sort for data

--- Nasr/Student/main.cpp
+++ Nasr/Student/main.cpp
@@ -1,5 +1,5 @@
 #include<iostream>
-#include"models/student.hpp"
+#include"test173.hpp"
 #include<string>
 #include<list>
 
@@ -9,11 +9,21 @@ int main()
 {
    Student *ptr;
    ptr= new Student;
-   ptr->setName("Mohammad")->setCode(1)->setAverage(18)->add();
-   ptr->setName("Ali")->setCode(2)->setAverage(19.5)->add();
-   ptr->setName("Hasan")->setCode(3)->setAverage(20)->add();
-   ptr->setName("Jvad")->setCode(2)->setAverage(19)->add();
+   ptr->setName("Mohammad Nasr")->setCode(1)->setAverage(19)->add();
+   ptr->setName("Ali Hoseini")->setCode(2)->setAverage(19.5)->add();
+   ptr->setName("Hasan Akbari")->setCode(3)->setAverage(20)->add();
+   ptr->setName("Jvad Mansoori")->setCode(4)->setAverage(19)->add();
+   ptr->setName("Fatemeh Mohammadi")->setCode(5)->setAverage(12)->add();
+   ptr->setName("Ali Hoseini")->setCode(11)->setAverage(10)->add();
    ptr->list();
+   cout<<endl;
+   cout<<"---------------------"<<endl;
+   ptr->setName("Ali Hoseini")->filter(Field::Name)->list(true);
+   //ptr->setName("Mahmood sadeghi")->setCode(6)
+   //->setAverage(19)->filter(Field::Average)->list(true);
+
+
+/*
    cout<<"***************"<<endl<<endl;
    ptr->setName("Hosein")->setAverage(14)->setCode(1)->remove()->list();
    ptr->setCode(1)->remove();
@@ -21,6 +31,7 @@ int main()
    {
       cout<<ptr->getError()<<endl;
    }
+   */
   // if(ptr->setName("naghi")->setCode(1)->setAverage(15)->add()->fail()==false)
   // {
   //    cout<<"Success"<<endl;

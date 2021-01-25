99079be687d9495bc40fd1271bceddaec01c8aa2
Hossein Pakparvar <hp.pakparvar@gmail.com>
2021-01-25 19:12:03 +0330

    Add some changes

--- Pakparvar/Student/main.cpp
+++ Pakparvar/Student/main.cpp
@@ -8,34 +8,50 @@ using namespace std;
 
 int main()
 {
+    system("cls");
     Student* student;
     student = new Student;
     student->setCode(1)->setName("Hossein")->setAverage(19)->add();
     student->setCode(2)->setName("Ali")->setAverage(20)->add();
     student->setCode(3)->setName("Mohammad")->setAverage(17)->add();
-    cout << endl <<"----------------------------------------" << endl;
     
+    
+    cout << endl << "-----------------------------------------------------" << endl;
+    student->sort(Field::Average, SortMode::Desc)->limit(1)->list();
+    cout << endl << "-----------------------------------------------------" << endl;
+
+    cout << endl << "-----------------------------------------------------" << endl;
+    student->sort(Field::Average)->limit(1)->list();
+    cout << endl << "-----------------------------------------------------" << endl;
+
+
+
+
+
+
+
+
+
+
     //student->sort(Field::Average, SortMode::Desc)->list();
     //student->sort(Field::Name)->list();
-    student->setAverage(20)->filter(Field::Average)->sort(Field::Name)->list(true);
+    //student->setAverage(20)->filter(Field::Average)->sort(Field::Name)->list(true);
    
-    cout << endl <<"----------------------------------------" << endl;
+   // cout << endl <<"----------------------------------------" << endl;
 
 
+   //student->setAverage(20)->filter(Field::Average)->list(true);
 
 
-
-
-
-
-
-    cout << endl <<"----------------------------------------" << endl;
+    //cout << endl <<"----------------------------------------" << endl;
     
-    student->setAverage(20)->filter(Field::Average)->list(true);
+    //student->setAverage(20)->filter(Field::Average)->first(true)->debug();
 
-    cout << endl << "---------------------------------------" << endl;
+    //cout << endl << "---------------------------------------" << endl;
 
-    student->setName("Reza")->filter(Field::Name)->list(true);
+   
+   
+    //student->setName("Reza")->filter(Field::Name)->list(true);
 
 
 
